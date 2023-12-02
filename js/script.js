043d5a08d712ba70281195dcf9b74000c1edc124
Octuber Sahn <octuberksahn@gmail.com>
2023-12-02 12:35:15 -0600

    nav, home and about me complete

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const overlay = document.querySelector(".overlay");
 const modal = document.querySelector(".modal");
 const badge = document.querySelector(".badge");
+const modalClose = document.querySelector(".close-btn");
 
 const SHOW_CLASS = "show";
 const HIDE_CLASS = "hide";
 
 badge.addEventListener('click', () => {
     overlay.classList.remove("hide");
+    overlay.classList.add("show");
+  })
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.remove("show");
   })
