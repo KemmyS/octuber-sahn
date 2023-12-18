7d171f7a96b58e9ef77b8b53e9e10cb8c79786a0
Octuber Sahn <octuberksahn@gmail.com>
2023-12-18 16:59:18 -0600

    polish changes

--- js/script.js
+++ js/script.js
@@ -5,9 +5,6 @@ const modalClose = document.querySelector(".close-btn");
 const menu = document.querySelector(".menu");
 const nav = document.querySelector(".main-nav");
 
-const formInput = document.querySelectorAll(".contact_input");
-
-
 const hasClass = (element, className) => element.classList.contains(className);
 const addClass = (element, className) => element.classList.add(className);
 const removeClass = (element, className) => element.classList.remove(className);
@@ -16,45 +13,37 @@ const SHOW_CLASS = "show";
 const HIDE_CLASS = "hide";
 const ACTIVE_CLASS = "active";
 
-openModal.addEventListener('click', () => {
-    overlay.classList.remove("hide");
-    overlay.classList.add("show");
-  })
-
-modalClose.addEventListener('click', () => {
-    overlay.classList.remove(SHOW_CLASS);
-    overlay.classList.add(HIDE_CLASS);
-  })
-
-
-  menu.addEventListener('click', (e) => {
-    const item = e.target; 
-
-    if(!hasClass(menu, ACTIVE_CLASS)) {
-        //show the navbar
-        addClass(menu, ACTIVE_CLASS);
-        addClass(nav, SHOW_CLASS);
-        removeClass(nav, HIDE_CLASS);
-    } else {
-        //hide the navbar
-        removeClass(menu, ACTIVE_CLASS);
-        removeClass(nav, SHOW_CLASS);
-        addClass(nav, HIDE_CLASS);
-    }
-
+openModal.addEventListener("click", () => {
+  removeClass(overlay, HIDE_CLASS);
+  addClass(overlay, SHOW_CLASS);
 });
 
-const mediaQuery = window.matchMedia("(min-width: 768px)")
+modalClose.addEventListener("click", () => {
+  removeClass(overlay, SHOW_CLASS);
+  addClass(overlay, HIDE_CLASS);
+});
 
+menu.addEventListener("click", (e) => {
+  if (!hasClass(menu, ACTIVE_CLASS)) {
+    //show the navbar
+    addClass(menu, ACTIVE_CLASS);
+    addClass(nav, SHOW_CLASS);
+    removeClass(nav, HIDE_CLASS);
+  } else {
+    //hide the navbar
+    removeClass(menu, ACTIVE_CLASS);
+    removeClass(nav, SHOW_CLASS);
+    addClass(nav, HIDE_CLASS);
+  }
+});
 
+const mediaQuery = window.matchMedia("(min-width: 768px)");
 const handleMediaQuery = (event) => {
   if (event.matches) {
-      removeClass(nav,HIDE_CLASS);
-      addClass(nav,SHOW_CLASS);
-    }
+    removeClass(nav, HIDE_CLASS);
+    addClass(nav, SHOW_CLASS);
+  }
 };
 
-mediaQuery.addEventListener('change', handleMediaQuery);
-
+mediaQuery.addEventListener("change", handleMediaQuery);
 handleMediaQuery(mediaQuery);
-
